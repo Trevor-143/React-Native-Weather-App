61e2fc19a61c2542d1d386ac81a3ceb87c695182
Trevor_143 <trevorarapu2@gmail.com>
2023-10-31 14:07:44 +0300

    finished counter test

--- src/demo/Counter.jsx
+++ src/demo/Counter.jsx
@@ -1,25 +1,36 @@
-import react from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import react, { useState } from "react"
+import { View, Text, Button, StyleSheet } from "react-native"
 
 export default function Counter() {
-    let count = 0
- return (
-    <View style={styles.container} >
-        <Text style={styles.title} >{`count: ${count}`}</Text>
-        <Button color={'red'} title={'increase count'} onPress={() => {count++, console.log(count)}} />
-        <Button color={'green'} title={'decrease count'} onPress={() => {count--, console.log(count)}} />
+  // let count = 0
+  const [count, setCount] = useState(0)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Button
+        color={"red"}
+        title={"increase count"}
+        onPress={() => setCount(count + 1)}
+      />
+      <Button
+        color={"green"}
+        title={"decrease count"}
+        onPress={() => setCount(count - 1)}
+      />
     </View>
- )
+  )
 }
 
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'orange'
-    },
-    title: {
-        alignSelf: 'center',
-        fontSize: 25,
-        marginTop: 25
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "orange"
+  },
+  title: {
+    alignSelf: "center",
+    fontSize: 25,
+    marginTop: 25
+  }
 })
