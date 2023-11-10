acf87e242c42877ce25111e9233d10496e91b811
Trevor_143 <trevorarapu2@gmail.com>
2023-11-10 18:28:41 +0300

    3:45

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-import Counter from "./src/demo/Counter";
-
+// import Counter from "./src/demo/Counter";
+import { ActivityIndicator } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as Location from 'expo-location'
 
 export default function App() {
+
+  const [ loading, setLoading ] = useState(true)
+  const [ location, setLocation ] = useState(null)
+  const [ error, setError ] = useState(null)
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if(location) {
+    console.log(location)
+  }
+
+  if(loading) {
+    return (
+      <View style={styles.container} >
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+
   return(
     <NavigationContainer>
-      <Counter />
+      {/* <Counter /> */}
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
--- src/demo/Counter.jsx
+++ src/demo/Counter.jsx
@@ -1,13 +1,23 @@
-import react, { useState } from "react"
+import react, { useState, useEffect } from "react"
 import { View, Text, Button, StyleSheet } from "react-native"
 
 export default function Counter() {
   // let count = 0
   const [count, setCount] = useState(0)
+  const [newCount, setNewCount] = useState(0)
+
+  useEffect(() => {
+    console.log(`count changed ${count}`)
+    return () => {
+      console.log('useeffect cleanup')
+    }
+  }, [count])
+  
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{`count: ${count}`}</Text>
+      <Text style={styles.title}>{`new count: ${newCount}`}</Text>
       <Button
         color={"red"}
         title={"increase count"}
@@ -17,6 +27,16 @@ export default function Counter() {
         color={"green"}
         title={"decrease count"}
         onPress={() => setCount(count - 1)}
+      />
+      <Button
+        color={"red"}
+        title={"increase count"}
+        onPress={() => setNewCount(newCount + 1)}
+      />
+      <Button
+        color={"green"}
+        title={"decrease count"}
+        onPress={() => setNewCount(newCount - 1)}
       />
     </View>
   )
